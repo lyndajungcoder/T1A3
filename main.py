ba705b1f78c44bd12d85a0e95fdafc2f6051482f
lyndajungcoderacademy <15231@coderacademy.edu.au>
2024-04-28 17:27:32 +1000

    csv file create and adding in schedule

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ from colored import Fore, Back, style
 class Scheduler:
     def __init__(self):
         self.schedule = {}
+
+# csv file
+        if os.path.isfile("schedule.csv"):
+            with open("schedule.csv", "r") as file:
+                csv_reader = csv.reader(file)
+                for row in csv_reader:
+                    hour, task = row
+                    self.schedule[int(hour)] = task
+
+# to add in the schedule
+
+    def add_schedule(self, hour, task):
+        if hour in self.schedule:
+            color: str = f"{Style.BOLD}{fore.RED}{Back.BLACK}"
+
+            print(f"You already have a task for {hour}:00.")
